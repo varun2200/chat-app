ec23196b950ceaa1161bbc17e342dfc7912a14d5
Varun <shahvarun391@gmail.com>
2025-08-03 21:41:01 +0530

    updated image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,12 @@ import LoginPage from "./pages/LoginPage"
 import ProfilePage from "./pages/ProfilePage"
 import  { Toaster } from "react-hot-toast"
 import { AuthContext } from "../context/AuthContext"
+import bgImage from "./assets/bgImage.svg"
 
 function App() {
   const {authUser} = useContext(AuthContext);
   return (
-      <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+      <div style={{backgroundImage: `url(${bgImage})`}} className = "bg-contain">
         <Toaster />
         <Routes>
           <Route path="/" element = {authUser ?<HomePage /> : <Navigate to='/login' />}/>
