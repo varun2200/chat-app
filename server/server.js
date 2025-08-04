493781ac176c12e79198251fa2e5125b30dcbf7a
Varun <shahvarun391@gmail.com>
2025-08-04 13:04:06 +0530

    server issue resolved

--- server/server.js
+++ server/server.js
@@ -40,5 +40,10 @@ app.use("/api/message", messageRouter)
 
 
 await connectDB();
-const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+
+if(process.env.NODE_ENV !== "production"){
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT,()=>console.log(`Server is running on port ${PORT}`));
+}
+
+export default server;
